55ef61e3561ada22affe444367f53a59d63c1360
Alethea Butler <alethea@alethea.io>
2014-04-21 22:49:28 -0400

    Test for sha256

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "loader.h"
+#include "hash.h"
 
 int main() {
     char *data;
     size_t length;
+    char *digest;
+    int i;
     
     load("main.c", &data, &length);
-    printf("%s\n%d\n", data, (int)length);
-    return EXIT_SUCCESS;
+    digest = sha256(data, length);
+
+    printf("main.c: ");
+    for (i = 0; i < SHA256_LENGTH; i++) {
+        printf("%02hhx", digest[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
