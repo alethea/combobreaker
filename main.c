4d3d4fd2925b7207b8f9484530513c0ebce238ca
Alethea Butler <alethea@alethea.io>
2014-05-11 20:11:31 -0400

    CPU-based main program

--- main.c
+++ main.c
@@ -1,16 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "hash.h"
 #include "alphabet.h"
 
 int main() {
-    int i;
+    char *test;
+    char *ref;
+    int match;
     alphabet_iter *iter;
 
+    match = 1;
     iter = make_alphabet_iter(8);
-    for (i = 0; i < 8192; i++) {
-        puts(alphabet_next(iter));
+    ref = scan_hash(SHA256_LENGTH);
+
+    while (match) {
+        alphabet_next(iter);
+        test = sha256(iter->buf, iter->cur + 1);
+        match = memcmp(test, ref, SHA256_LENGTH);
+    }
+
+    if (!match) {
+        puts(iter->buf);
     }
     free_alphabet_iter(iter);
+    free(ref);
+    free(test);
+    if (match) {
+        return 1;
+    }
     return 0;
 }
